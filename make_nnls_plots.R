915a76b80800d49f74309b36dd5c31dfae53b3b1
Joshmoss11 <joshua.moss@mail.huji.ac.il>
2016-08-31 23:57:49 +0300

    functionalized

--- make_nnls_plots.R
+++ make_nnls_plots.R
@@ -1,20 +1,38 @@
 library(nnls)
 library(gplots)
+make_nnls_pies <- function(matrix.f,out.f){
+	x <- read.csv(matrix.f,row.names=1,check.names=F)
+	lab <- c(colnames(x),'Other')
+	pal <- rainbow(length(lab))
+	pdf(out.f)
+	x[x<0.005] <- 0
+	for (i in 1:nrow(x)){
+    	x.cur <- x[i,]
+    	pie(x.cur[x.cur!=0],labels=lab[x.cur!=0],col=pal[x.cur!=0],main=rownames(x)[i])
+	}
+	dev.off()
+}
+make_nnls_heatmap <- function(matrix.f,out.f){
+    x <- read.csv(matrix.f,row.names=1,check.names=F)
+	pdf(out.f)
+	heatmap.2(as.matrix(x),trace='none',margins = c(8, 8))
+	dev.off()
+}
+
 deconv.dir <- '/mnt/lustre/hms-01/fs01/joshua.moss/dor/deconvolution'
 nnls.dir <- file.path(deconv.dir,'nnls')
 plots.dir <- file.path(deconv.dir,'plots')
-dir.create(plots.dir)
-x <- read.csv(file.path(nnls.dir,'nnls_betas_norm.csv'),row.names=1,check.names=F)
-pdf(file.path(plots.dir,'betas_heatmap.pdf'))
-heatmap.2(as.matrix(x),trace='none',margins = c(8, 8))
-dev.off()
+dir.create(plots.dir,showWarnings=F)
+matrix.f <- file.path(nnls.dir,'nnls_betas_norm.csv')
+pies.f <- file.path(plots.dir,'betas_pies.pdf')
+heatmap.f <- file.path(plots.dir,'betas_heatmap.pdf')
 
-pal <- rainbow(length(lab))
-pdf(file.path(plots.dir,'betas_pies.pdf'))
-x[x<0.005] <- 0
-lab <- c(colnames(x),'Other')
-for (i in 1:nrow(x)){
-	x.cur <- x[i,]
-	pie(x.cur[x.cur!=0],labels=lab[x.cur!=0],col=pal[x.cur!=0],main=rownames(x)[i])
-}
-dev.off()
+matrix.f.w.c <- file.path(nnls.dir,'nnls_betas_norm_wi_cancer.csv')
+pies.f.w.c <- file.path(plots.dir,'betas_pies_wi_cancer.pdf')
+heatmap.f.w.c <- file.path(plots.dir,'betas_heatmap_wi_cancer.pdf')
+
+make_nnls_pies(matrix.f,pies.f)
+make_nnls_heatmap(matrix.f,heatmap.f)
+
+make_nnls_pies(matrix.f.w.c,pies.f.w.c)
+make_nnls_heatmap(matrix.f.w.c,heatmap.f.w.c)
--- run_nnls_matrix.R
+++ run_nnls_matrix.R
@@ -1,30 +1,47 @@
 nrows=-1
 library(nnls)
+nnls_matrix <- function(X,Y){
+	nans <- apply(X,1,function(x) {any(is.na(x))}) | apply(Y,1,function(x) {any(is.na(x))})
+	X <- X[!nans,]
+	Y <- Y[!nans,]
+	
+	N <- nrow(X); M <- ncol(X); K <- ncol(Y)
+
+	nnls.betas <- matrix(NA,K,M)
+	for (k in 1:K){
+		nnls.betas[k,] <- nnls(X,Y[,k])$x
+	}
+	rownames(nnls.betas) <- colnames(Y)
+	colnames(nnls.betas) <- colnames(X)
+	return(nnls.betas)
+}
+norm_nnls_betas <- function(nnls.betas){
+	nnls.betas.norm <- t(apply(nnls.betas,1,function(x) {x/sum(x)}))
+	rownames(nnls.betas.norm) <- colnames(Y)
+	colnames(nnls.betas.norm) <- colnames(X)
+}
+run_nnls_matrix <- function(X.f,Y.f,out.f,out.norm.f=NULL,normbetas=T,rm.cols=c('Vasc_endothelium','Skin',
+												'Liver','Omentum')){
+	X <- as.matrix(read.csv(X.f,row.names=1,nrow=nrows,check.names=F))[,!(colnames(X) %in% rm.cols)]
+	Y <- as.matrix(read.csv(Y.f,row.names=1,nrow=nrows,check.names=F))
+	nnls.betas <- nnls_matrix(X,Y)
+	write.csv(nnls.betas,out.f,row.names=T,quote=F)
+	if (normbetas){
+		write.csv(norm_nnls_betas(nnls.betas),out.norm.f,row.names=T,quote=F)
+	}
+}
+
 deconv.dir <- '/mnt/lustre/hms-01/fs01/joshua.moss/dor/deconvolution'
 matrices.dir <- file.path(deconv.dir,'matrices')
 nnls.dir <- file.path(deconv.dir,'nnls')
-dir.create(nnls.dir)
-X <- as.matrix(read.csv(file.path(matrices.dir,'ref_no_cancer.csv'),row.names=1,nrow=nrows,check.names=F))
-Y <- as.matrix(read.csv(file.path(matrices.dir,'test_samples.csv'),row.names=1,nrow=nrows,check.names=F))
+dir.create(nnls.dir,showWarnings=F)
+X.f <- file.path(matrices.dir,'ref_no_cancer.csv')
+Y.f <- file.path(matrices.dir,'test_samples.csv')
+out.f <- file.path(nnls.dir,'nnls_betas.csv')
+out.norm.f <- file.path(nnls.dir,'nnls_betas_norm.csv')
+X.f.w.c <- file.path(matrices.dir,'ref_wi_cancer.csv')
+out.f.w.c <- file.path(nnls.dir,'nnls_betas_wi_cancer.csv')
+out.norm.f.w.c <- file.path(nnls.dir,'nnls_betas_norm_wi_cancer.csv')
 
-rm.cols <- c('Vasc_endothelium')
-X <- X[,!(colnames(X) %in% rm.cols)]
-
-nans <- apply(X,1,function(x) {any(is.na(x))}) | apply(Y,1,function(x) {any(is.na(x))})
-X <- X[!nans,]
-Y <- Y[!nans,]
-
-N <- nrow(X); M <- ncol(X); K <- ncol(Y)
-
-nnls.betas <- matrix(NA,K,M)
-for (k in 1:K){
-	nnls.betas[k,] <- nnls(X,Y[,k])$x
-}
-rownames(nnls.betas) <- colnames(Y)
-colnames(nnls.betas) <- colnames(X)
-write.csv(nnls.betas,file.path(nnls.dir,'nnls_betas.csv'),row.names=T,quote=F)
-
-nnls.betas.norm <- t(apply(nnls.betas,1,function(x) {x/sum(x)}))
-rownames(nnls.betas.norm) <- colnames(Y)
-colnames(nnls.betas.norm) <- colnames(X)
-write.csv(nnls.betas.norm,file.path(nnls.dir,'nnls_betas_norm.csv'),row.names=T,quote=F)
+run_nnls_matrix(X.f,Y.f,out.f,out.norm.f)
+run_nnls_matrix(X.f.wc,Y.f,out.f.wc,out.norm.f.wc)
